7ba836cf8c62b4890b9c633b4053ece96d5be78b
matteobasso <matteobasso9@gmail.com>
2024-06-27 11:48:22 +0200

    bin tree esame

--- test.fsx
+++ test.fsx
@@ -6,10 +6,12 @@
 
 type 'a BinTree =
 
-    // empty type, like leaf = (value, Empty, Empty)
+    // empty type, like leaf = Node(value, Empty, Empty)
     | Empty
 
     // node type, for every node of the tree
+    // Node (value, leaf, Node(...))
+    // Node (value, Node(...), Node(...))
     | Node of 'a * 'a BinTree * 'a BinTree
 
 
@@ -43,54 +45,3 @@ let rec fold_tree tree func accu =
 
         // then return the value proccessed by the function passed by the user
         func (value, accu_right)
-
-
-
-
-
-
-
-
-
-
-
-type BinTree<'T> =
-
-    // empty type, like leaf = (value, Empty, Empty)
-    | Empty
-
-    // node type, for every node of the tree
-    | Node of 'T * BinTree<'T> * BinTree<'T>
-
-
-let rec map_tree tree func =
-    match tree with
-    | Empty -> Empty
-    | Node (value, left, right) ->
-        // apply func to the value
-        let new_val = func value
-
-        // apply to the left branch
-        let new_left = map_tree left func
-
-        // apply to the right branch
-        let new_right = map_tree right func
-
-        // return the new node
-        Node (new_val, new_left, new_right)
-
-
-let rec fold_tree tree func accu =
-    | Empty -> accu
-    | Node (value, left, right) ->
-
-        // first fold left branch
-        let accu_left = fold_tree (left, func, accu)
-
-        // then accumulate in the new value the left accumulator with the value of the fold of right branch
-        let accu_right = fold_tree (right, func, accu_left)
-
-        // then return the value proccessed by the function passed by the user
-        func (value, accu_right)
-
-
